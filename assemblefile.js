609be6e8cb2fc220aa857f8ed07cc6978e7fe7f8
J. Ky Marsh <jkymarsh@gmail.com>
2015-12-08 00:20:34 -0800

    revert to pre-gulp assemble; add sample hbs files

--- assemblefile.js
+++ assemblefile.js
@@ -1,8 +1,12 @@
 var assemble = require('assemble');
 var extname = require('gulp-extname');
 
+assemble.layouts('layouts/*.hbs');
+assemble.option('layout', 'default');
+
 assemble.task('html', function() {
   assemble.src('templates/*.hbs')
+    .pipe(assemble.renderFile())
     .pipe(extname())
     .pipe(assemble.dest('dist/'));
 });
